2e1c080b25dada1da8a7576c2ed30e2ee6b58c59
Parag <parag@ajency.in>
2017-12-01 13:23:49 +0530

    subscriber recipients  updated - array of objects

--- src/Ajency/communication/Dispatch.php
+++ src/Ajency/communication/Dispatch.php
@@ -70,18 +70,29 @@ class Dispatch
             case 'email':
                 $email = new EmailSubscriber();
 
-                /*If subscriber ids are given on email object */
+                /*If subscriber objects are given on email object */
                 $recipient_0   = $notification['recipients'][0];
                 $toSubscribers = $recipient_0->getToSubscribers();
 
                 if (count($toSubscribers) > 0) {
-                    $subscribers_emails = DB::table('user_communications')->select('value')->whereIn('object_id', $toSubscribers)->where('type', 'email')->get();
 
-                    foreach ($subscribers_emails as $subscriber_email) {
-                        $subscribers_emails_ids[] = $subscriber_email->value;
+                    foreach ($toSubscribers as $subscriber) {
+                        $sub_attributes    = $subscriber->getattributes();
+                        $subscribers_email = DB::table('user_communications')
+                            ->select('value')
+                            ->where('object_id', $sub_attributes['object_id'])
+                            ->where('type', $sub_attributes['type'])
+                            ->where('object_type', $sub_attributes['object_type'])
+                            ->where('is_communication', 1)
+                            ->where('is_primary', 1)
+                            ->first();
+
+                        if ($subscribers_email != false & !is_null($subscribers_email)) {
+                            $subscribers_emails_ids[] = $subscribers_email->value;
+                        }
+
                     }
                     $notification['recipients'][0]->setTo($subscribers_emails_ids);
-
                 }
 
                 $email_id = $notification['recipients'][0];
@@ -105,19 +116,30 @@ class Dispatch
                 $sms = new SmsSubscriber();
                 $job = $notification['recipients'][0];
 
-                /*if subscriber ids are provided on sms object */
+                /*if subscriber objects are provided on sms object */
                 $toSubscribers = $job->getToSubscribers();
 
-               
                 $subscribers_no_ids = [];
 
                 if (count($toSubscribers) > 0) {
-                    $subscribers_nos = DB::table('user_communications')->select('value')->whereIn('object_id', $toSubscribers)->where('type', 'sms')->get();
 
+                    foreach ($toSubscribers as $subscriber) {
+                        $sub_attributes = $subscriber->getattributes();
+                        $subscriber_no  = DB::table('user_communications')
+                            ->select('value')
+                            ->where('object_id', $sub_attributes['object_id'])
+                            ->where('type', $sub_attributes['type'])
+                            ->where('object_type', $sub_attributes['object_type'])
+                            ->where('is_communication', 1)
+                            ->where('is_primary', 1)
+                            ->first();
 
-                    foreach ($subscribers_nos as $subscriber_no) {
-                        $subscribers_no_ids[] = $subscriber_no->value;
+                        if ($subscriber_no != false & !is_null($subscriber_no)) {
+                            $subscribers_no_ids[] = $subscriber_no->value;
+                        }
+
                     }
+
                     $job->setTo($subscribers_no_ids);
 
                 }
