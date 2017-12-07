f8e6d99f8d0ac3e98f573a0f5cfe425a57c7d3a7
Parag <parag@ajency.in>
2017-12-07 10:23:56 +0530

    use event template sms

--- src/Ajency/providers/SmsGupshup.php
+++ src/Ajency/providers/SmsGupshup.php
@@ -3,10 +3,8 @@ namespace Ajency\Comm\Providers;
 
 use Ajency\Comm\Models\Error;
 use Ajency\Comm\Models\Log;
-
-
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\View;
 
 /*
  * SmsGupshup provider class
@@ -28,31 +26,51 @@ class SmsGupshup
             $log->setApi('SmsGupshup SMS'); //can get config from config() here - TODO
             $log->setRequest(serialize([]));
             $log->setNotificationData(serialize($notification));
-            // dd($notification);
-            $sms = $notification['sms_id'];
+
+            $sms      = $notification['sms_id'];
             $to_array = $sms->getTo();
             $response = [];
+
+            $sms_content = $sms->getMessage();
+
+            //if sms content is not given, get the default template set in configuration for the sms event
+            if (is_null($sms_content) || $sms_content == "") {
+
+                if (isset($notification['template_id'])) {
+
+                    $sms_view    = view::make($notification['template_id'], $sms->getData());
+                    $sms_content = $sms_view->render();
+                }
+            }
+
             foreach ($to_array as $to) {
-                $link = 'http://enterprise.smsgupshup.com/GatewayAPI/rest?';
+                $link  = 'http://enterprise.smsgupshup.com/GatewayAPI/rest?';
                 $array = [
-                    'method' => 'SendMessage',
-                    'send_to'=> $to,
-                    'msg' => $sms->getMessage(),
-                    'userid' => $notification['provider_params']['username'],
+                    'method'      => 'SendMessage',
+                    'send_to'     => $to,
+                    'msg'         => $sms_content,
+                    'userid'      => $notification['provider_params']['username'],
                     'auth_scheme' => 'plain',
-                    'password' => $notification['provider_params']['password'],
-                    'v' => '1.1',
-                    'format'=>'text',
+                    'password'    => $notification['provider_params']['password'],
+                    'v'           => '1.1',
+                    'format'      => 'text',
                 ];
-                if($sms->getOverride()) $array['override_dnd'] = 'true';
+                if ($sms->getOverride()) {
+                    $array['override_dnd'] = 'true';
+                }
+
                 $link .= http_build_query($array);
+
                 $ch = curl_init($link);
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                $response[$to] = ['response' => curl_exec($ch), 'request' => $link ];
+                $response[$to] = ['response' => curl_exec($ch), 'request' => $link];
                 curl_close($ch);
-                if(explode(' ',$response[$to]['response'])[0] == 'error') throw new \Exception(serialize($response[$to]));
+                if (explode(' ', $response[$to]['response'])[0] == 'error') {
+                    throw new \Exception(serialize($response[$to]));
+                }
+
             }
-            
+
             $log->setUserId(Auth::id());
             $log->setResponse(serialize([$response]));
             $log->save();
--- src/models/SmsRecipient.php
+++ src/models/SmsRecipient.php
@@ -1,50 +1,80 @@
-<?php 
+<?php
 namespace Ajency\Comm\Models;
 
-class SmsRecipient{
-	const TYPE = 'sms';
+class SmsRecipient
+{
+    const TYPE = 'sms';
 
-	private $to = [];
-	private $to_subscribers = [];
-	private $message;
-	private $override_dnd = false;
-	
+    private $to             = [];
+    private $to_subscribers = [];
+    private $message;
+    private $override_dnd = false;
+    private $data         = [];
+    private $attributes   = ['data' => []];
 
-	public function setTo($to){
-		$this->to = (is_array($to))? $to : [$to];
-	}
+    public function setTo($to)
+    {
+        $this->to = (is_array($to)) ? $to : [$to];
+    }
 
-	public function setToSubscribers($to){
-		$this->to_subscribers = (is_array($to))? $to : [$to];
-	}
+    public function setToSubscribers($to)
+    {
+        $this->to_subscribers = (is_array($to)) ? $to : [$to];
+    }
 
-	public function setOverride($override){
-		$this->override_dnd = $override;
-	}
+    public function setOverride($override)
+    {
+        $this->override_dnd = $override;
+    }
 
-	public function getTo(){
-		return $this->to ;
-	}
+    public function getTo()
+    {
+        return $this->to;
+    }
 
-	public function getToSubscribers(){
-		return $this->to_subscribers ;
-	}	
+    public function getToSubscribers()
+    {
+        return $this->to_subscribers;
+    }
 
-	public function addTo($to){
-		$this->to[] = $to;
-	}
-	
+    public function addTo($to)
+    {
+        $this->to[] = $to;
+    }
 
-	public function setMessage($message){
-		$this->message = $message;
-	}
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
 
-	public function getMessage(){
-		return $this->message;
-	}
+    public function getMessage()
+    {
+        return $this->message;
+    }
 
-	public function getOverride(){
-		return $this->override_dnd;
-	}
+    public function getOverride()
+    {
+        return $this->override_dnd;
+    }
+
+    public function setData($data)
+    {
+        $this->data = $data;
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    public function setAttributes($data)
+    {
+        $this->attribures = $data;
+    }
+
+    public function getAttributes()
+    {
+        return $this->attributes;
+    }
+
 }
-
